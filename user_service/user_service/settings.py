1b4a2f133f62455db5f0946a2a83655d8edf78a8
clashhulk <clash.paradox99@gmail.com>
2025-03-23 20:31:36 +0530

    cross origin added on render

--- user_service/user_service/settings.py
+++ user_service/user_service/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:3000", "https://communication-notice-app.onrender.com"
 ]
 
 # Application definition
