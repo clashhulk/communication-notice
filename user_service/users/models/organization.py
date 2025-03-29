ea6c1010f6e2a44bd7af288e9183f8d6d0e76675
clashhulk <clash.paradox99@gmail.com>
2025-03-29 17:59:49 +0530

    organization int8 to uuid primery key data type changed

--- user_service/users/models/organization.py
+++ user_service/users/models/organization.py
@@ -1,7 +1,9 @@
 from django.db import models
+import uuid
 
 
 class Organization(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True)
     address = models.TextField(blank=True, null=True)
     phone = models.CharField(max_length=15, blank=True, null=True)
